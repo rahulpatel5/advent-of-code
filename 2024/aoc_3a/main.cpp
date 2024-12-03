a7c70fe741a453683470ef7a6ac8e2e18562166b
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-03 22:44:50 +0000

    2024 aoc3a | add solution

---
+++ 2024/aoc_3a/main.cpp
@@ -0,0 +1,25 @@
+#include "aoc3a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr auto lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)))"};
+    
+    int result {aoc3a::parseAndMultiply(lines)};
+    std::cout << "Final result: " << result << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
