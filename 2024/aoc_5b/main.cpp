d6046d3e70df293c2e7eccadba6bf3ac28b04aae
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-06 00:13:19 +0000

    2024 aoc5b | add solution

---
+++ 2024/aoc_5b/main.cpp
@@ -0,0 +1,55 @@
+#include "aoc5b.h"
+#include "../aoc_5a/input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    int sum {aoc5b::parseAndSumMiddlePages<length>(lines)};
+    std::cout << "The sum of middle pages is: " << sum << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
