f56a081d33bd156529c858e67188a2ebabba2c87
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-09 00:32:13 +0000

    2024 aoc8a | add solution

---
+++ 2024/aoc_8a/main.cpp
@@ -0,0 +1,39 @@
+#include "aoc8a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    int count {aoc8a::parseAndCountAntinodes<length>(lines)};
+    std::cout << "number of antinodes: " << count << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
