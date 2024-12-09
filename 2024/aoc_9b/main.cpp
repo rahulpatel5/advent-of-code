36d70683bc23c8676ea8ed8db1ee7b70e87dca1a
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-09 17:16:43 +0000

    2024 aoc9b | add solution

---
+++ 2024/aoc_9b/main.cpp
@@ -0,0 +1,27 @@
+#include "aoc9b.h"
+#include "../aoc_9a/input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(2333133121414131402)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    long long checksum {aoc9b::parseAndGetChecksum(input)};
+    std::cout << "checksum is: " << checksum << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
