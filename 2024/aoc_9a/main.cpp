a9ef95649e0be9c7ccf926583ecc7200012e902d
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-09 11:59:51 +0000

    2024 aoc9a | add solution

---
+++ 2024/aoc_9a/main.cpp
@@ -0,0 +1,22 @@
+#include "aoc9a.h"
+#include "input.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    
+    constexpr std::string_view testInput {R"(2333133121414131402)"};
+    
+    long long checksum {aoc9a::parseAndGetChecksum(input)};
+    std::cout << "checksum is: " << checksum << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
