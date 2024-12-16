bb040c67698409dfebb18ab6e1785f39aeadfc5f
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-16 16:38:37 +0000

    2024 aoc16a | add initial draft solution (no answer yet)

---
+++ 2024/aoc_16a/aoc16a.h
@@ -0,0 +1,206 @@
+#ifndef AOC16A_H
+#define AOC16A_H
+
+#include <string_view>
+#include <array>
+#include "../../shared/Direction.h"
+#include <vector>
+#include <set>
+#include <cassert>
+
+#include <iostream>
+
+/*
+iterate each possible path, with +1000 score for turn and +1 for moving
+collect lowest score
+where a path exceeds the lowest score, or reaches a dead end, stop the path
+return the lowest score
+*/
+
+namespace aoc16a
+{
+    using Move = std::pair<int, int>;
+    using Moves = std::vector<Move>;
+
+    using Position = std::pair<size_t, size_t>;
+    using Positions = std::vector<Position>;
+    using UniquePositions = std::vector<std::set<Position>>;
+
+    using Scores = std::vector<int>;
+
+    template <std::size_t N>
+    Position getStartingPosition(const std::array<std::string_view, N> map)
+    {
+        for (size_t y{0}; y < map.size(); ++y)
+        {
+            for (size_t x{0}; x < map[y].size(); ++x)
+            {
+                if (map[y][x] == 'S')
+                    return {x, y};
+            }
+        }
+        throw std::invalid_argument("Could not find starting position.\n");
+    }
+
+    Position goInDirection(Position pos, Move move)
+    {
+        return {pos.first + move.first, pos.second + move.second};
+    }
+
+    template <std::size_t N>
+    bool hitWall(Position pos, const std::array<std::string_view, N> map)
+    {
+        return map[pos.second][pos.first] == '#';
+    }
+
+    template <std::size_t N>
+    bool reachExit(Position pos, const std::array<std::string_view, N> map)
+    {
+        return map[pos.second][pos.first] == 'E';
+    }
+
+    template <std::size_t N>
+    bool goFreeSpace(Position pos, const std::array<std::string_view, N> map)
+    {
+        return map[pos.second][pos.first] == '.';
+    }
+
+    template <std::size_t N>
+    Moves getOtherMoves(Position pos, Move move, const std::array<std::string_view, N>& map)
+    {
+        Moves next {};
+        // check clockwise
+        Move clockMove {Direction::turnClockwise(move)};
+        Position clockPos {goInDirection(pos, clockMove)};
+        if (goFreeSpace(clockPos, map) || reachExit(clockPos, map))
+            next.push_back(clockMove);
+        //check anti-clockwise
+        Move antiClockMove {Direction::turnAntiClockwise(move)};
+        Position antiClockPos {goInDirection(pos, antiClockMove)};
+        if (goFreeSpace(antiClockPos, map) || reachExit(antiClockPos, map))
+            next.push_back(antiClockMove);
+
+        return next;
+    }
+
+    void deleteNonValidPaths(Positions& positions, Moves& movements, Scores& scores, UniquePositions& uniquePos, int index)
+    {
+        positions.erase(std::next(positions.begin(), index), std::next(positions.begin(), index + 1));
+        movements.erase(std::next(movements.begin(), index), std::next(movements.begin(), index + 1));
+        scores.erase(std::next(scores.begin(), index), std::next(scores.begin(), index + 1));
+        uniquePos.erase(std::next(uniquePos.begin(), index), std::next(uniquePos.begin(), index + 1));
+    }
+
+    bool isPositionAlreadyReached(Position pos, const std::set<Position>& uniquePositions)
+    {
+        for (Position uniquePosition : uniquePositions)
+        {
+            if (uniquePosition == pos)
+                return true;
+        }
+        return false;
+    }
+
+    int getLowestScore(const Scores& scores)
+    {
+        int lowest {};
+        for (int score : scores)
+        {
+            if (lowest == 0 || score < lowest)
+                lowest = score;
+        }
+        return lowest;
+    }
+
+    template <std::size_t N>
+    int parseAndGetScore(const std::array<std::string_view, N>& lines)
+    {
+        int score {0};
+        constexpr Move move {Direction::east}; // set in puzzle
+        Position start {getStartingPosition<N>(lines)};
+
+        // set up starting position
+        Positions positions {};
+        positions.push_back(start);
+        Moves movements {};
+        movements.push_back(move);
+        Scores scores {};
+        scores.push_back(0);
+        UniquePositions uniquePos {};
+        std::set<Position> startSet {start};
+        uniquePos.push_back(startSet);
+
+        Scores finalScores {};
+        bool noPathsLeft {false};
+        while (!noPathsLeft)
+        {
+            std::vector<size_t> nonValidPaths {};
+            assert((positions.size() == movements.size() && movements.size() == scores.size() && scores.size() == uniquePos.size()) && "Info on the number of paths is inconsistent (position, move, score, unique positions).\n");
+            for (size_t i{0}; i < positions.size(); ++i)
+            {
+                Position currentPos {positions[i]};
+                Move currentMove {movements[i]};
+                int currentScore {scores[i]};
+                std::set<Position> currentUniquePos {uniquePos[i]};
+                // first traverse the current direction
+                Position forward {goInDirection(positions[i], movements[i])};
+                // check that position wasn't reached already
+                // i.e. avoid loops, or paths that are longer than needed
+                if (isPositionAlreadyReached(forward, uniquePos[i]))
+                    nonValidPaths.push_back(i);
+                // otherwise path is valid
+                else if (goFreeSpace(forward, lines))
+                {
+                    positions[i] = forward;
+                    ++scores[i];
+                    uniquePos[i].insert(forward);
+                }
+                // if new position is exit, then path is done
+                else if (reachExit(positions[i], lines))
+                {
+                    finalScores.push_back(++scores[i]);
+                    nonValidPaths.push_back(i);
+                }
+                // if hit wall, then current path is done
+                else if (hitWall(forward, lines))
+                    nonValidPaths.push_back(i);
+
+                // next look at alternate paths, i.e. (anti-)clockwise
+                Moves alternateMoves {getOtherMoves(currentPos, currentMove, lines)};
+                for (Move otherMove : alternateMoves)
+                {
+                    Position nextSpace {goInDirection(currentPos, otherMove)};
+                    // check that position was not reached before
+                    if (isPositionAlreadyReached(nextSpace, currentUniquePos))
+                        continue;
+                    // if path valid, then add as a new path
+                    else if (goFreeSpace(nextSpace, lines))
+                    {
+                        positions.push_back(nextSpace);
+                        movements.push_back(otherMove);
+                        // add 1,000 for turn and 1 for move
+                        scores.push_back(currentScore + 1001);
+                        std::set<Position> newUnique {currentUniquePos};
+                        newUnique.insert(nextSpace);
+                        uniquePos.push_back(newUnique);
+                    }
+                    // if at exit, add 1,000 for turn and 1 for move
+                    else if (reachExit(nextSpace, lines))
+                        finalScores.push_back(currentScore + 1001);
+                }
+            }
+
+            // clean up by removing non-valid paths
+            // go in reverse order to delete the right paths
+            for (int j{static_cast<int>(nonValidPaths.size() - 1)}; j >= 0; --j)
+                deleteNonValidPaths(positions, movements, scores, uniquePos, nonValidPaths[j]);
+            
+            if (positions.size() == 0)
+                noPathsLeft = true;
+        }
+
+        return getLowestScore(scores);
+    }
+}
+
+#endif // AOC16A_H
---
+++ 2024/aoc_16a/main.cpp
@@ -0,0 +1,42 @@
+#include "aoc16a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    int score {aoc16a::parseAndGetScore<testLength>(testLines)};
+    std::cout << "score: " << score << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
