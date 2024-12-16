d6f31fbe154b1b0102efa85ffa247d3590cdbc5d
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-16 16:39:10 +0000

    shared | add direction header file for moving in compass directions

---
+++ shared/Direction.h
@@ -0,0 +1,43 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+#include <array>
+#include <stdexcept>
+
+namespace Direction
+{
+    constexpr std::pair<int, int> north { 0, -1};
+    constexpr std::pair<int, int> east  { 1,  0};
+    constexpr std::pair<int, int> south { 0,  1};
+    constexpr std::pair<int, int> west  {-1,  0};
+    
+    constexpr std::pair<int, int> turnClockwise(std::pair<int, int> move)
+    {
+        if (move == north)
+            return east;
+        else if (move == east)
+            return south;
+        else if (move == south)
+            return west;
+        else if (move == west)
+            return north;
+        else
+            throw std::invalid_argument("Given non-valid initial direction.\n");
+    }
+    
+    constexpr std::pair<int, int> turnAntiClockwise(std::pair<int, int> move)
+    {
+        if (move == north)
+            return west;
+        else if (move == east)
+            return north;
+        else if (move == south)
+            return east;
+        else if (move == west)
+            return south;
+        else
+            throw std::invalid_argument("Given non-valid initial direction.\n");
+    }
+}
+
+#endif // DIRECTION_H
