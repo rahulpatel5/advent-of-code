a16eda855a756d0a4d6c773d6faa09f4af6cea74
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2024-12-19 01:09:20 +0000

    2024 aoc14b | add solution

---
+++ 2024/aoc_14b/main.cpp
@@ -0,0 +1,41 @@
+#include "aoc14b.h"
+#include "../aoc_14a/input.h"
+#include "../../shared/StringUtils.h"
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    auto startTime {std::chrono::high_resolution_clock::now()};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    constexpr std::pair<int, int> map {101, 103};
+    
+    constexpr std::string_view testInput {R"(p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    constexpr std::pair<int, int> testMap {11, 7};
+    
+    int treeTime {aoc14b::parseAndGetTreeTime<length>(lines, map)};
+    std::cout << "time to tree: " << treeTime << '\n';
+    
+    auto stopTime {std::chrono::high_resolution_clock::now()};
+    const std::chrono::duration<double> durationSeconds {stopTime - startTime};
+    std::cout << "Time taken: " << durationSeconds.count() << " s\n";
+    
+    return 0;
+}
