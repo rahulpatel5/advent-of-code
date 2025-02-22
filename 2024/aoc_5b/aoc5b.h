715136e491a4fa5a6a3a488215e324605e9df832
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-02-22 20:02:00 +0000

    2024 aoc5b | add new 5a solution as a starter

--- 2024/aoc_5b/aoc5b.h
+++ 2024/aoc_5b/aoc5b.h
@@ -3,127 +3,96 @@
 
 #include <string_view>
 #include <array>
+#include <string>
+#include <map>
 #include <vector>
-#include <sstream>
+#include <set>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
 
 /*
-read order rules
-read page updates
-iterate each set of page updates
-for a page update set, iterate rules to check if page update is valid
-discard valid updates and collect non-valid ones
-run through non-valid updates and correct them
-(note: if making code more efficient, try to combine the above two steps)
-check that corrected non-valid updates now work
-sum middle figures for corrected non-valid updates
+re-use faster part 1 solution
+flip logic to get incorrectly ordered updates
+correct ordering for those and get middle page
 */
 
 namespace aoc5b
 {
-    template <std::size_t N>
-    std::vector<std::pair<int, int>> readOrderRules(std::array<std::string_view, N> lines)
-    {
-        std::vector<std::pair<int, int>> rules {};
-        
-        for (std::string_view line : lines)
-        {
-            if (line.find('|') == std::string_view::npos)
-                continue;
-
-            size_t pipe {line.find('|')};
-            int num1 {std::stoi(std::string(line.substr(0, pipe)))};
-            int num2 {std::stoi(std::string(line.substr(pipe + 1)))};
-            rules.push_back({num1, num2});
-        }
-
-        return rules;
-    }
+    using Page = int;
+    using UniquePages = std::set<Page>;
+    using Rules = std::map<Page, UniquePages>;
+    using Updates = std::vector<Page>;
+    using UpdateLists = std::vector<Updates>;
 
     template <std::size_t N>
-    std::vector<std::vector<int>> readPageUpdates(std::array<std::string_view, N> lines)
+    void readRulesAndUpdates(Rules& rules, UpdateLists& updates, const std::array<std::string_view, N>& lines)
     {
-        std::vector<std::vector<int>> updates {};
-
-        for (std::string_view line : lines)
+        for (const std::string_view& line : lines)
         {
-            if (line.find(',') == std::string_view::npos)
-                continue;
-            
-            size_t start {0};
-            size_t end {0};
-            std::vector<int> innerVec {};
-
-            while (end != std::string_view::npos)
+            // collect rules if there's a '|' in the line
+            if (line.find('|') != std::string_view::npos)
             {
-                end = line.find(',', start);
-                int num {std::stoi(std::string(line.substr(start, end - start)))};
-                innerVec.push_back(num);
-                start = end + 1;
+                size_t pipe {line.find('|')};
+                int num1 {std::stoi(std::string(line.substr(0, pipe)))};
+                int num2 {std::stoi(std::string(line.substr(pipe + 1)))};
+                rules[num1].insert(num2);
             }
 
-            updates.push_back(innerVec);
-        }
-
-        return updates;
-    }
-
-    bool isUpdateNotValid(std::vector<int> update, std::vector<std::pair<int, int>> rules)
-    {
-        for (const std::pair<int, int>& rule : rules)
-        {
-            bool activeSearch {false};
-            for (int number : update)
+            // collect updates if there's a ',' in the line
+            else if (line.find(',') != std::string_view::npos)
             {
-                if (rule.second == number)
-                    activeSearch = true;
-                if ((activeSearch) && (rule.first == number))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    int getMiddleNumber(std::vector<int> update)
-    {
-        size_t middle {(update.size() - 1) / 2};
-        return update.at(middle);
-    }
-
-    std::vector<int> fixNonValidUpdate(std::vector<int> nonValidUpdate, std::vector<std::pair<int, int>> rules)
-    {
-        std::vector<int> fixedUpdate {nonValidUpdate};
-        for (const std::pair<int, int>& rule : rules)
-        {
-            bool activeSearch {false};
-            std::size_t laterNum {};
-            for (std::size_t i{0}; i < fixedUpdate.size(); ++i)
-            {
-                if (rule.second == fixedUpdate[i])
+                size_t index {0};
+                size_t comma {};
+                Updates inner {};
+                while (comma != std::string_view::npos)
                 {
-                    activeSearch = true;
-                    laterNum = i;
+                    comma = line.find(',', index);
+                    int num {std::stoi(std::string(line.substr(index, comma)))};
+                    inner.push_back(num);
+                    index = comma + 1;
                 }
-                if ((activeSearch) && (rule.first == fixedUpdate[i]))
-                {
-                    fixedUpdate[i] = nonValidUpdate[laterNum];
-                    fixedUpdate[laterNum] = nonValidUpdate[i];
-                    return fixNonValidUpdate(fixedUpdate, rules);
-                }
+                updates.push_back(inner);
             }
         }
-        return fixedUpdate;
     }
 
-    int applyRulesToUpdates(std::vector<std::pair<int, int>> rules, std::vector<std::vector<int>> updates)
+    int getMiddleNumber(const Updates& updates)
+    {
+        assert(updates.size() % 2 != 0 && "Error: got an unexpected even number of page numbers while looking for middle page.\n");
+        size_t middleIndex {(updates.size() - 1) / 2};
+        return updates.at(middleIndex);
+    }
+
+    int findCorrectUpdatesAndGetMiddleNumber(const Rules& rules, const UpdateLists& updateLists)
     {
         int sum {0};
 
-        for (std::size_t i{0}; i < updates.size(); ++i)
+        for (const Updates& updates : updateLists)
         {
-            if (isUpdateNotValid(updates[i], rules))
+            UniquePages previous {};
+            bool isCorrectOrder {true};
+            for (const Page& page : updates)
             {
-                std::vector<int> fixedUpdate {fixNonValidUpdate(updates[i], rules)};
-                sum += getMiddleNumber(fixedUpdate);
+                // check if any previous elements are in rules for current
+                // rules for current element are those that should be after
+                // if they're before then the order is incorrect
+                UniquePages intersection;
+                std::set_intersection(previous.begin(), previous.end(), 
+                                      rules.at(page).begin(), rules.at(page).end(),
+                                      std::inserter(intersection, intersection.begin()));
+                if (!intersection.empty())
+                {
+                    isCorrectOrder = false;
+                    break;
+                }
+
+                previous.insert(page);
+            }
+
+            if (isCorrectOrder)
+            {
+                sum += getMiddleNumber(updates);
             }
         }
 
@@ -131,12 +100,13 @@ namespace aoc5b
     }
 
     template <std::size_t N>
-    int parseAndSumMiddlePages(std::array<std::string_view, N> lines)
+    int parseAndSumMiddlePages(const std::array<std::string_view, N>& lines)
     {
-        std::vector<std::pair<int, int>> orderRules {readOrderRules<N>(lines)};
-        std::vector<std::vector<int>> pageUpdates {readPageUpdates<N>(lines)};
+        Rules orderRules {};
+        UpdateLists pageUpdates {};
+        readRulesAndUpdates<N>(orderRules, pageUpdates, lines);
 
-        return applyRulesToUpdates(orderRules, pageUpdates);
+        return findCorrectUpdatesAndGetMiddleNumber(orderRules, pageUpdates);
     }
 }
 
