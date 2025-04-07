0f7cac07c1cf4ec8a5d96fa26357c5bb15ec6844
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-04-07 20:51:21 +0100

    2024 aoc9a | add faster solution

--- 2024/aoc_9a/aoc9a.h
+++ 2024/aoc_9a/aoc9a.h
@@ -8,12 +8,13 @@
 /*
 process string into vector (because integers can go above single digits)
 rearrange vector to move later numbers to fill earlier 'empty' ones
+keep track of left position, to not waste time like an idiot
 calculate checksum
 */
 
 namespace aoc9a
 {
-    std::vector<int> getIndividualBlocks(std::string_view input)
+    std::vector<int> getIndividualBlocks(const std::string_view& input, size_t& finalBlockPosition)
     {
         std::vector<int> blocks {};
         int fileID {0};
@@ -30,35 +31,36 @@ namespace aoc9a
                     blocks.push_back(free);
             }
             if (i % 2 == 0)
+            {
+                finalBlockPosition += size;
                 ++fileID;
+            }
         }
 
         return blocks;
     }
 
-    std::vector<int> defragmentSystem(const std::vector<int>& blocks)
+    std::vector<int> defragmentSystem(const std::vector<int>& blocks, size_t finalBlockPosition)
     {
         std::vector<int> processed {blocks};
+        long long currentPosition {0};
 
-        for (size_t i{0}; i < processed.size(); ++i)
+        for (long long i{static_cast<long long>(processed.size() - 1)}; i >= 0; --i)
         {
+            if (i == finalBlockPosition)
+                break;
+
             if (processed[i] == -1)
+                continue;
+            
+            for (; currentPosition < static_cast<long long>(processed.size()); ++currentPosition)
             {
-                size_t lastIndex {};
-                bool indexFound {false};
-                for (size_t j{processed.size() - 1}; j > i; --j)
+                if (processed.at(currentPosition) == -1)
                 {
-                    if (processed[j] != -1)
-                    {
-                        lastIndex = j;
-                        indexFound = true;
-                        break;
-                    }
-                }
-                if (indexFound)
-                {
-                    processed[i] = processed[lastIndex];
-                    processed[lastIndex] = -1;
+                    processed[currentPosition] = processed[i];
+                    processed[i] = -1;
+                    ++currentPosition;
+                    break;
                 }
             }
         }
@@ -66,7 +68,7 @@ namespace aoc9a
         return processed;
     }
 
-    long long getChecksum(std::vector<int> filesystem)
+    long long getChecksum(const std::vector<int>& filesystem)
     {
         long long sum {0};
         int position {-1};
@@ -82,10 +84,11 @@ namespace aoc9a
         return sum;
     }
 
-    long long parseAndGetChecksum(std::string_view input)
+    long long parseAndGetChecksum(const std::string_view& input)
     {
-        std::vector<int> blocks {getIndividualBlocks(input)};
-        std::vector<int> filesystem {defragmentSystem(blocks)};
+        size_t finalBlockPosition {0};
+        std::vector<int> blocks {getIndividualBlocks(input, finalBlockPosition)};
+        std::vector<int> filesystem {defragmentSystem(blocks, finalBlockPosition)};
 
         return getChecksum(filesystem);
     }
