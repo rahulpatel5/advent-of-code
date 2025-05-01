b89bba2fab0834905f2f1d608cb306f2a764ffff
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-05-01 19:39:06 +0100

    2015 aoc13a | add solution

---
+++ 2015/aoc_13a/aoc13a.h
@@ -0,0 +1,121 @@
+#ifndef AOC13A_H
+#define AOC13A_H
+
+#include <array>
+#include <string_view>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+/*
+collect units for each person (from their perspective)
+iterate each arrangement and the total happiness units
+return the optimum result
+*/
+
+namespace aoc13a
+{
+    using HapInt = long long;
+    using HapUnit = int;
+
+    using PersonName = std::string;
+    using Person = int;
+    using PersonMap = std::map<PersonName, Person>;
+    using PeoplePair = std::pair<Person, Person>;
+    using HappyMap = std::map<std::pair<Person, Person>, HapUnit>;
+
+    // order of people on table. Doesn't connect first and last
+    using TableOrder = std::vector<Person>;
+
+    template <std::size_t N>
+    int processInput(const std::array<std::string_view, N>& lines, HappyMap& happyMap)
+    {
+        Person personIndex {0};
+        PersonMap people {};
+
+        for (std::string_view line : lines)
+        {
+            // collecting names
+            size_t firstSpace {line.find(' ')};
+            PersonName firstName {line.substr(0, firstSpace)};
+            if (people.find(firstName) == people.end())
+            {
+                people[firstName] = personIndex;
+                ++personIndex;
+            }
+
+            size_t lastSpace {line.find_last_of(' ')};
+            size_t fullStop {line.find('.')};
+            PersonName lastName {line.substr(lastSpace + 1, fullStop - (lastSpace + 1))};
+            if (people.find(lastName) == people.end())
+            {
+                people[lastName] = personIndex;
+                ++personIndex;
+            }
+
+            // collecting happiness units
+            size_t secondSpace {line.find(' ', firstSpace + 1)};
+            size_t thirdSpace {line.find(' ', secondSpace + 1)};
+            size_t fourthSpace {line.find(' ', thirdSpace + 1)};
+
+            HapUnit happ {std::stoi(std::string(line.substr(thirdSpace + 1, fourthSpace - (thirdSpace + 1))))};
+            PeoplePair pair {people.at(firstName), people.at(lastName)};
+
+            bool isPositive {line.substr(secondSpace + 1, thirdSpace - (secondSpace + 1)) == "gain"};
+            if (!isPositive)
+                happ = -happ;
+
+            happyMap[pair] = happ;
+        }
+
+        return personIndex;
+    }
+
+    HapInt maxHappiness(int n, const HappyMap& happyMap)
+    {
+        HapInt max {};
+
+        TableOrder order {};
+        for (int i{0}; i < n; ++i)
+            order.push_back(i);
+
+        do
+        {
+            HapInt happy {0};
+            for (size_t n{0}; n < order.size(); ++n)
+            {
+                PeoplePair pair {};
+                // pair on one side
+                if (n == 0)
+                    pair = {order.at(n), order.back()};
+                else
+                    pair = {order.at(n), order.at(n - 1)};
+                happy += happyMap.at(pair);
+
+                // pair on other side
+                if (n == order.size() - 1)
+                    pair = {order.at(n), order.front()};
+                else
+                    pair = {order.at(n), order.at(n + 1)};
+                happy += happyMap.at(pair);
+            }
+            if (happy > max)
+                max = happy;
+        }
+        while (std::next_permutation(order.begin(), order.end()));
+
+        return max;
+    }
+
+    template <std::size_t N>
+    HapInt getHappy(const std::array<std::string_view, N>& lines)
+    {
+        HappyMap happyMap {};
+        int numberPeople {processInput<N>(lines, happyMap)};
+
+        return maxHappiness(numberPeople, happyMap);
+    }
+}
+
+#endif // AOC13A_H
---
+++ 2015/aoc_13a/main.cpp
@@ -0,0 +1,37 @@
+#include "aoc13a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include "../../shared/Timer.h"
+#include <iostream>
+
+int main()
+{
+    Timer timer {};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+    
+    constexpr std::string_view testInput {R"(Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    aoc13a::HapInt happy {aoc13a::getHappy<length>(lines)};
+    std::cout << "Total happiness units: " << happy << '\n';
+    
+    std::cout << "Time taken: " << timer.getDuration() << " s\n";
+    
+    return 0;
+}
