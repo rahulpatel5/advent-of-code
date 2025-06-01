aa0fbddcad2a3b2389939025e168ed5a37c8fa1b
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-06-01 19:48:03 +0100

    2015 aoc22a | add solution

---
+++ 2015/aoc_22a/main.cpp
@@ -0,0 +1,21 @@
+#include "aoc22a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include "../../shared/Timer.h"
+#include <iostream>
+
+int main()
+{
+    Timer timer {};
+    
+    constexpr std::string_view boss {input::input};
+    constexpr std::size_t bossLength {StringUtils::length(boss)};
+    constexpr std::array<std::string_view, bossLength> bossInfo {StringUtils::split<bossLength>(boss)};
+    
+    aoc22a::Mana mana {aoc22a::getLeastManaToWin<bossLength>(bossInfo)};
+    std::cout << "The least mana to win: " << mana << '\n';
+    
+    std::cout << "Time taken: " << timer.getDuration() << " s\n";
+    
+    return 0;
+}
