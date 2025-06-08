93cbe292ee943713605d4bff38dadcbe6bb3baca
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-06-08 20:37:57 +0100

    2015 aoc24a | add solution

---
+++ 2015/aoc_24a/main.cpp
@@ -0,0 +1,36 @@
+#include "aoc24a.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include "../../shared/Timer.h"
+#include <iostream>
+
+int main()
+{
+    Timer timer {};
+    
+    constexpr std::string_view input {input::input};
+    constexpr std::size_t length {StringUtils::length(input)};
+    constexpr std::array<std::string_view, length> lines {StringUtils::split<length>(input)};
+
+    constexpr std::string_view testInput {R"(1
+2
+3
+4
+5
+7
+8
+9
+10
+11
+)"};
+    constexpr std::size_t testLength {StringUtils::length(testInput)};
+    constexpr std::array<std::string_view, testLength> testLines {StringUtils::split<testLength>(testInput)};
+    
+    constexpr int numberGroups {3};
+    aoc24a::Quantum quantum {aoc24a::getPresentEntanglement<length, numberGroups>(lines)};
+    std::cout << "Quantum entanglement: " << quantum << '\n';
+    
+    std::cout << "Time taken: " << timer.getDuration() << " s\n";
+    
+    return 0;
+}
