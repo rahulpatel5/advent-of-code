7e3e8cfbc721ff605281999b787f62a6ade18573
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2026-04-03 14:43:14 +0100

    2016 aoc8a | add solution

---
+++ 2016/aoc_8a/src/aoc_8a/core.clj
@@ -0,0 +1,85 @@
+(ns aoc-8a.core)
+(require 'clojure.string)
+
+;; APPLY each instruction
+;; RETURN count of lit pixels
+
+(def test-input "rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1")
+(def input-file (slurp "input.txt"))
+;; choose which input to use
+;; (def input test-input)
+(def input input-file)
+
+(defn clean-and-split-input [input]
+  (-> input
+      (clojure.string/replace #"\r" "")
+      (clojure.string/split #"\n")))
+
+(defn set-up-screen [size]
+  (vec (repeat (second size) (vec (repeat (first size) 0)))))
+
+(defn get-coordinates [col row]
+  (map #(vector %1 %2) (flatten (repeat (range row))) (mapcat #(repeat row %) (range col))))
+
+;; assumes rectangle isn't larger than screen
+(defn apply-rect [screen col row]
+  (vec (reduce #(assoc-in %1 %2 1) screen (get-coordinates col row))))
+
+(defn get-rect-args [shape screen]
+  (let [shp (clojure.string/split shape #"x")
+        col (Integer/parseInt (first shp))
+        row (Integer/parseInt (second shp))]
+    (apply-rect screen col row)))
+
+;; assumes shift positive and is less than element length
+(defn shifted-element [element shift]
+  (take (count element) (drop (- (count element) shift) (cycle element))))
+
+(defn rotate-row [screen row shift]
+  (assoc screen row (vec (shifted-element (get screen row) shift))))
+
+(defn get-col-coord [screen col]
+  (map #(vector % col) (range (count screen))))
+
+(defn get-col-element [screen col]
+  (map #(get-in screen %) (get-col-coord screen col)))
+
+(defn rotate-col [screen col shift element]
+  (reduce #(assoc-in %1 (first %2) (second %2)) screen (map vector (get-col-coord screen col) (shifted-element element shift))))
+
+(defn get-rot-args [index shift screen]
+  (let [inf (clojure.string/split index #"=")
+        dir (first inf)
+        idx (Integer/parseInt (second inf))
+        sft (Integer/parseInt shift)]
+    (case dir
+      "x" (rotate-col screen idx sft (get-col-element screen idx))
+      "y" (rotate-row screen idx sft)
+      (throw (Exception. "Did not get valid rotation instruction.")))))
+
+(defn apply-instructions [screen instruction]
+  (let [[a b c _ e] (clojure.string/split instruction #" ")]
+    (cond
+      (= a "rect") (get-rect-args b screen)
+      (= a "rotate") (get-rot-args c e screen)
+      :else (throw (Exception. "Received unknown instruction.")))))
+
+(defn get-lit-pixels [input]
+  (let [cleaned-input (clean-and-split-input input)
+        screen-size [50 6]
+        screen (set-up-screen screen-size)
+        final-screen (reduce apply-instructions screen cleaned-input)]
+    (reduce #(reduce + %1 %2) 0 final-screen)))
+
+(defn -main []
+  (let [start (System/nanoTime)
+        pixels (get-lit-pixels input)
+        end (System/nanoTime)
+        elapsed (/ (- end start) 1e9)]
+    (println (format "Elapsed: %.6f s" elapsed))
+    (println "The number of lit pixels is:" pixels)))
+
+;; (-main)
